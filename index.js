1d2dc3f3efbec8e4f9a8d5fbc5e9d7f619e1d07f
Hrishikesh <hrishipotdar23@gmail.com>
2018-07-09 18:29:07 +0530

    linting and test cases passed

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
+const removeDuplicateItems = (items) => {
+  const expectedItems = [];
+  for (let i = 0; i < items.length; i += 1) {
+    if (expectedItems.indexOf(items[i]) === -1) {
+      expectedItems.push(items[i]);
+    }
+  }
+  return expectedItems;
+};
 
 module.exports = removeDuplicateItems;
